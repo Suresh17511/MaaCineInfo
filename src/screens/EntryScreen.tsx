0564fd4f740b8ce5cef6fa280198b0793356bfec
SureshChidurala <suresh@fabstudio.co>
2022-07-02 12:39:34 +0530

    #0005 Added tailwind css

--- src/screens/EntryScreen.tsx
+++ src/screens/EntryScreen.tsx
@@ -1,20 +1,27 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { AuthStackParams, StackScreenProps } from '../../types';
+import { tailwind } from '../utils/tailwind';
 
 interface EntryScreenProps
     extends StackScreenProps<AuthStackParams, 'EntryScreen'> {}
 
 const EntryScreen: React.FC<EntryScreenProps> = ({ navigation }) => {
     return (
-        <View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('LoginScreen')}
-                >
-                    <Text>Login Screen</Text>
-                </TouchableOpacity>
-            </View>
+        <View
+            style={tailwind(
+                'w-full',
+                'h-full',
+                'flex',
+                'justify-center',
+                'items-center'
+            )}
+        >
+            <TouchableOpacity
+                onPress={() => navigation.navigate('LoginScreen')}
+            >
+                <Text style={tailwind('text-primary')}>Login Screen</Text>
+            </TouchableOpacity>
         </View>
     );
 };
